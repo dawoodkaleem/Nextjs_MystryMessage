7cafed508bac04f81ae4db5232a4b1f54f05627b
dawoodkaleem <dawood9743@gmail.com>
2025-01-02 19:12:17 +0500

    component created  user data types and interface created

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,4 +1,4 @@
-import mongoose,{Schema,Document, Mongoose} from "mongoose";
+import mongoose,{Schema,Document,Mongoose} from "mongoose";
 
  export interface Message extends Document{
   content:string;
