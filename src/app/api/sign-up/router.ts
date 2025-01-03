20ae9f0794964478d411af696492df550f12198a
dawoodkaleem <dawood9743@gmail.com>
2025-01-03 17:41:44 +0500

    Updated

---
+++ src/app/api/sign-up/router.ts
@@ -0,0 +1,24 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
+sendVerificationEmail;
+
+export async function POST(request:Request){
+  await dbConnect();
+  try {
+    const {username,email,password  }=await request.json()
+  } catch (error) {
+    console.error("Error registring user",error )
+    return Response.json({
+      success:false,
+      message:"Error registring user"
+    },
+    {
+      status:500
+    }
+  
+  )
+    
+  }
+}
