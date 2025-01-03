ebd0713babf5610ced69d136f97162b975bc57fd
dawoodkaleem <dawood9743@gmail.com>
2025-01-03 17:49:06 +0500

    updated

--- src/app/api/sign-up/router.ts
+++ src/app/api/sign-up/router.ts
@@ -8,6 +8,17 @@ export async function POST(request:Request){
   await dbConnect();
   try {
     const {username,email,password  }=await request.json()
+   const existingUserVerifiedByUsername= await UserModel.findOne({
+      username,
+      isVerified:true
+    })
+    if(existingUserVerifiedByUsername){
+      return Response.json({
+success:false,
+message:"Username is already taken"
+      },{status:400})
+    }
+    
   } catch (error) {
     console.error("Error registring user",error )
     return Response.json({
